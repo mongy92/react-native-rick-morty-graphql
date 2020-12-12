268c4fc0127da156d02e3e456629526f49a4a254
Ahmed Mongy <a.elzantot@gmail.com>
2020-12-12 04:02:31 +0200

    create three empty screens (Splash - CharacterList - CharachterDetails)

---
+++ src/screens/CharacterDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CharacterDetails = () => {
+  return (
+    <View>
+      <Text>CharacterDetails</Text>
+    </View>
+  );
+};
+
+export default CharacterDetails;
