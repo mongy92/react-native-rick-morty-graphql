0b37133d036f2d05ef9bdc3f25e0714bea873e6e
Ahmed Mongy <a.elzantot@gmail.com>
2020-12-12 04:12:21 +0200

    handle nagiation between CharacterList and CharacterDetails with params (id - name)

--- src/screens/CharacterDetails/index.tsx
+++ src/screens/CharacterDetails/index.tsx
@@ -1,10 +1,14 @@
+import { RouteProp, useRoute } from '@react-navigation/native';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { MainStackParams } from '../../navigation/MainStack';
 
 const CharacterDetails = () => {
+  const { params } = useRoute<RouteProp<MainStackParams, 'CharacterDetails'>>();
   return (
     <View>
       <Text>CharacterDetails</Text>
+      <Text>{`Name : ${params.name}`}</Text>
     </View>
   );
 };
--- src/screens/CharacterList/index.tsx
+++ src/screens/CharacterList/index.tsx
@@ -1,10 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 const CharacterList = () => {
+  const navigation = useNavigation();
+
+  const onPressCharacter = () => {
+    navigation.navigate('CharacterDetails', {
+      id: 1,
+      name: 'Ahmed Mongy',
+    });
+  };
   return (
     <View>
       <Text>CharacterList</Text>
+      <Button title="Details" onPress={onPressCharacter} />
     </View>
   );
 };
